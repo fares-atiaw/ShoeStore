3a5dd0ed2daee8552b3fe6a1df2b32b09d30ce47
fares <0fareshassan0@gmail.com>
2022-07-28 18:05:51 +0200

    Needs ↓ to be finished : 1) if(s == null),  however it went to else ?! 2) Navigation Graph is right and worked before, but after some modifies ..?!?!..

--- app/src/main/java/com/example/shoestore_starter/ui/activities/MainActivity.kt
+++ app/src/main/java/com/example/shoestore_starter/ui/activities/MainActivity.kt
@@ -16,22 +16,28 @@ import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.example.shoestore_starter.R
 import com.example.shoestore_starter.databinding.ActivityMainBinding
+import com.example.shoestore_starter.ui.fragments.F_Login
+import com.example.shoestore_starter.ui.fragments.F_LoginDirections
 
 class MainActivity : AppCompatActivity() {
 private lateinit var binding: ActivityMainBinding
 private lateinit var navController: NavController
+private lateinit var config: AppBarConfiguration
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
-//        setSupportActionBar(binding.toolbar)
-        navController = binding.myNavHostFragment.getFragment<NavHostFragment>().navController
-        val config = AppBarConfiguration(navController.graph)
-
         setSupportActionBar(binding.toolbar)
+
+        navController = binding.myNavHostFragment.getFragment<NavHostFragment>().navController
         NavigationUI.setupActionBarWithNavController(this, navController)
+        config = AppBarConfiguration(navController.graph)
+
+//        setSupportActionBar(binding.toolbar)
+
         NavigationUI.setupWithNavController(binding.toolbar, navController)
         binding.toolbar.setupWithNavController(navController, config) // soon
+//        setupActionBarWithNavController(navController, config)
 
         // Add menu items without overriding methods in the Activity
         addMenuProvider(object : MenuProvider {
@@ -39,11 +45,12 @@ private lateinit var navController: NavController
                 // Add menu items here
                 menuInflater.inflate(R.menu.main_mainu, menu)
             }
-
             override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
 //                navController.popBackStack(R.id.f_Welcome, false)
                 NavigationUI.onNavDestinationSelected(menuItem, binding.myNavHostFragment.findNavController())
-//                navController.popBackStack(R.id.f_Welcome, true)
+                navController.navigate("F_Login")
+                navController.popBackStack()
+
 //                navController.popBackStack(R.id.f_Login, false)
                 return true
             }
