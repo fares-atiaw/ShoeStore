eb4fb0f4016999bd80e05b3bd4367ed86fe9dd7c
fares <0fareshassan0@gmail.com>
2022-07-28 19:43:42 +0200

    Needs ↓ to be finished : 1) if(s == null),  however it went to else ?! 2) Navigation Graph is right and worked before, but after some modifies ..?!?!..

--- app/src/main/java/com/example/shoestore_starter/ui/fragments/F_Login.kt
+++ app/src/main/java/com/example/shoestore_starter/ui/fragments/F_Login.kt
@@ -3,6 +3,7 @@ package com.example.shoestore_starter.ui.fragments
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -34,8 +35,8 @@ class F_Login : Fragment() {
             override fun afterTextChanged(s: Editable?) {}
             override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
             override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-                if(!(binding.etEmail.text.isEmpty() || binding.etPassword.text.isEmpty()))
-                    binding.btnLogin.isEnabled = true
+                binding.btnLogin.isEnabled = !(binding.etEmail.text.isEmpty() || binding.etPassword.text.isEmpty())
+//                Log.w("email and password => \n", "${viewModel.email.value}\n${viewModel.password.value}")
             }
         }
         return binding.root
